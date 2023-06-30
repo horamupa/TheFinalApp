11d0edd99473df1bec4421f40471cc8dfaaea220
horamupa <horamupa@gmail.com>
2023-06-30 14:23:50 +0300

    Initial Commit

---
+++ TheFinalApp/TheFinalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheFinalAppApp.swift
+//  TheFinalApp
+//
+//  Created by MM on 30.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TheFinalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
